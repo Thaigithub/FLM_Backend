c32f6a6de3919333f51e4adcbc0af0c416388fcc
LamVAnAM <namxp2@gmail.com>
2023-06-09 14:01:26 +0700

    uddate auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -44,11 +44,7 @@ export class AuthService {
                 userId: user.id,
                 role: user.role
             },{
-<<<<<<< HEAD
-                // expiresIn:''
-=======
                 expiresIn: '8h',
->>>>>>> 266f367d25b07e3abd15a91da9d497c3ddab9a99
                 secret: this.configService.get('JWT_SECRET')
             })
             return {
